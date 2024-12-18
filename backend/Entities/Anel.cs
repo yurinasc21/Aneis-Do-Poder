b0a5672126b2255b51866165a724d0052156543d
Yuri Rocha <yurirocha15nascimento@outlook.com>
2024-12-18 16:46:55 -0300

    Criacao Entities

---
+++ backend/Entities/Anel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace ForjandoAneis.Entities
+{
+    public class Anel
+    {
+
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string Poder { get; set; }
+        public string Portador { get; set; }
+        public string ForjadoPor { get; set; }
+        public string Imagem { get; set; }
+    }
+}
